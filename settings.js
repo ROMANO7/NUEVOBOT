f4f5a8b048e2a01f8b1aaa19b3220f3cc970fd51
ROMANO7 <89288883+ROMANO7@users.noreply.github.com>
2022-09-12 01:08:36 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -65,9 +65,9 @@ global.owner = ['14256892344'],
 	       ['13607277031']
 global.ownertag = '14256892344' //ur tag number
 global.botname = 'INDOBOT' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
+global.linkz = "https://youtu.be/Wi" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtu.be/Wi" //ur website to be displayed
+global.botscript = 'https://github.com/' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
 global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
